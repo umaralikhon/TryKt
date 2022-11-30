f3e71a6e58677a90f3a52658a9b75482068c96d2
Umar <umaralikhon@gmail.com>
2022-11-30 15:44:19 +0500

    OOP

---
+++ src/main/kotlin/com/example/trykt/ktclasses/Employee.kt
@@ -0,0 +1,6 @@
+package com.example.trykt.ktclasses
+
+class Employee: Person {
+    constructor(firstname: String, lastname: String, age: Int) : super(firstname, lastname, age);
+
+}
--- src/main/kotlin/com/example/trykt/ktclasses/Person.kt
+++ src/main/kotlin/com/example/trykt/ktclasses/Person.kt
@@ -1,3 +1,33 @@
 package com.example.trykt.ktclasses
 
-class Person(var firstname: String, var lastname: String, var age: Int);
+import lombok.Data
+import lombok.NoArgsConstructor
+
+@Data
+open class Person constructor(firstname: String, lastname: String, age: Int) {
+    constructor() : this("", "", 0);
+    constructor(firstname: String) : this("", "", 0);
+
+    var firstname: String = "None";
+    var lastname: String = "None";
+    var age: Int = 0;
+
+    private var email: String = "18"
+        set(email) {
+            field = email;
+        }
+        get():String {
+            return this.email;
+        }
+
+    init {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    init {
+        this.lastname = lastname;
+        this.age = age;
+    }
+}
